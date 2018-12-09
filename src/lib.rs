305334f5c8050009f5a62ba33cd5dc5df51d0c55
Avi ד <avi.the.coder@gmail.com>
2018-12-08 19:46:29 -0500

    Handle Multiple defs

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,16 @@
 macro_rules! whered {
-    ($n:ident = $e:expr) => {
-        macro_rules! $n {
-            () => {
-                $e
-            };
-        }
-    };
-    ($n:ident $arg_name:ident = $e:expr) => {
-        macro_rules! $n {
-            ($arg_expr:expr) => {{
-                let $arg_name = $arg_expr;
+    ($($n:ident $($arg_name:ident)* = $e:expr)+) => {
+        $( macro_rules! $n {
+            ($($arg_name: $arg_expr:expr)*) => {{
+                $(let $arg_name = $arg_expr;)*
                 $e
             }};
-        }
+        })+
     };
 }
 
 #[test]
-fn whered_macro() {
+fn map_plus_one() {
     let nums_plus1 = vec![2, 3, 4];
 
     let nums = [1, 2, 3];
@@ -31,7 +24,35 @@ fn whered_macro() {
 
     whered! { plus_one i = i.map(|n| *n + 1) };
 
-    assert_eq!(nums_plus1, plus_one!(iter).collect::<Vec<_>>());
-    assert_eq!(nums_plus1, plus_one!(iter_mut).collect::<Vec<_>>());
-    assert_eq!(nums_plus1, plus_one!(into_iter).collect::<Vec<_>>());
+    assert_eq!(nums_plus1, plus_one!(i: iter).collect::<Vec<_>>());
+    assert_eq!(nums_plus1, plus_one!(i: iter_mut).collect::<Vec<_>>());
+    assert_eq!(nums_plus1, plus_one!(i: into_iter).collect::<Vec<_>>());
+}
+
+#[test]
+fn no_arg() {
+    whered! { foo = "foo"};
+    assert_eq!("foo", foo!())
+}
+
+// FIXME
+#[test]
+fn two_args() {
+    whered! {
+        add n1 n2 = {n1 + n2}
+    }
+}
+
+#[test]
+fn multiple_definitions() {
+    whered! {
+        one = 1
+        two = 2
+        one_plus_two = one!() + two!()
+
+        two_plus n = n + 2
+    }
+
+    assert_eq!(3, one_plus_two!());
+    assert_eq!(4, two_plus!(n: 2));
 }
